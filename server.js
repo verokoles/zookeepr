e10e48dcc719e26be43fea6f13ea5602bf32dfdf
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-04-12 15:38:25 -0500

    add aniamls and zookeepr, public files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // const { type } = require('express/lib/response');
-
+app.use(express.static('public'));
 //parse uncoming string or array data
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSOON data
@@ -111,7 +111,15 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
